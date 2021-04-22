61de37e1dc7e931744023be9c60b280ef078d076
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-04-22 20:53:16 +0200

    Added floater to deco elements

---
+++ Assets/Scripts/Floater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    private float angle = 0;
+    private float sinValue;
+    private int frequency = 0;
+    private float initialYPosition;
+
+    public float FrequencyModifier = 1.0f;
+    public float AmplitudeModifier = 0.2f;
+    public float RotationSpeed = 1.0f;
+    public bool Rotate = false;
+
+
+    void Start()
+    {
+        initialYPosition = transform.position.y;
+    }
+
+    void Update()
+    {
+        // Move the object up and down in a sine wave
+        frequency++;
+        angle = (float)(Mathf.PI * frequency * FrequencyModifier / 180.0);
+        sinValue = Mathf.Sin(angle) * AmplitudeModifier;
+        transform.position = new Vector3(transform.position.x, initialYPosition + sinValue, transform.position.z);
+
+        // Rotate if asked
+        if (Rotate)
+        {
+            transform.Rotate(0.0f, RotationSpeed, 0.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/FloaterAssigner.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class FloaterAssigner : MonoBehaviour
+{
+    async void Start()
+    {
+        // Add the Floater.cs script to all top level children
+        foreach (Transform child in transform)
+        {
+            if (child != transform)
+            child.gameObject.AddComponent<Floater>();
+            await Task.Run(()=> Thread.Sleep(50));
+        }
+    }
+}
