6abc9485e307ca15f42fde19f7498cee98ed156d
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-05-27 22:21:52 +0200

    modified save data

--- Assets/Scripts/SaveData/SaveData.cs
+++ Assets/Scripts/SaveData/SaveData.cs
@@ -2,9 +2,21 @@
 public class SaveData
 {
     public int UnlockedLevel;
+    public int TotalCollectedCoins;
+
+    public SavedSettings Settings;
 
     public SaveData()
     {
 
     }
+}
+
+
+[System.Serializable]
+public class SavedSettings
+{
+    public bool IsMusicMuted;
+    public bool IsSoundEffectsMuted;
+    public bool IsScreenRotationEnabled;
 }
