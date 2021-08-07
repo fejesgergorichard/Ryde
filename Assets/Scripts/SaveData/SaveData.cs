3bc00af6febc4ebf3022a1b7b4cb70d90b7efcbc
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-08-07 21:30:16 +0200

    Added system using to savesystem

--- Assets/Scripts/SaveData/SaveData.cs
+++ Assets/Scripts/SaveData/SaveData.cs
@@ -1,22 +1,27 @@
-[System.Serializable]
-public class SaveData
+using System;
+
+namespace SaveData
 {
-    public int UnlockedLevel;
-    public int TotalCollectedCoins;
-
-    public SavedSettings Settings;
-
-    public SaveData()
+    [Serializable]
+    public class SaveData
     {
+        public int UnlockedLevel;
+        public int TotalCollectedCoins;
 
+        public SavedSettings Settings;
+
+        public SaveData()
+        {
+
+        }
     }
-}
 
 
-[System.Serializable]
-public class SavedSettings
-{
-    public bool IsMusicMuted;
-    public bool IsSoundEffectsMuted;
-    public bool IsScreenRotationEnabled;
+    [Serializable]
+    public class SavedSettings
+    {
+        public bool IsMusicMuted;
+        public bool IsSoundEffectsMuted;
+        public bool IsScreenRotationEnabled;
+    }
 }
--- Assets/Scripts/SaveData/SaveSystem.cs
+++ Assets/Scripts/SaveData/SaveSystem.cs
@@ -2,42 +2,46 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
-public static class SaveSystem
+namespace SaveData
 {
-    private static string path = Application.persistentDataPath + "/save.dat";
 
-    public static void Save(SaveData data)
+    public static class SaveSystem
     {
-        FileStream stream = new FileStream(path, FileMode.Create);
+        private static string path = Application.persistentDataPath + "/save.dat";
 
-        GetBinaryFormatter().Serialize(stream, data);
-
-        stream.Close();
-    }
-
-    public static SaveData Load()
-    {
-        if (File.Exists(path))
+        public static void Save(SaveData data)
         {
-            FileStream stream = new FileStream(path, FileMode.Open);
-            SaveData data = GetBinaryFormatter().Deserialize(stream) as SaveData;
+            FileStream stream = new FileStream(path, FileMode.Create);
+
+            GetBinaryFormatter().Serialize(stream, data);
+
             stream.Close();
-
-            return data;
         }
-        else
+
+        public static SaveData Load()
         {
-            Debug.LogError($"Save file not found in {path}.");
-            
-            return null;
+            if (File.Exists(path))
+            {
+                FileStream stream = new FileStream(path, FileMode.Open);
+                SaveData data = GetBinaryFormatter().Deserialize(stream) as SaveData;
+                stream.Close();
+
+                return data;
+            }
+            else
+            {
+                Debug.LogError($"Save file not found in {path}.");
+
+                return null;
+            }
         }
-    }
 
-    public static BinaryFormatter GetBinaryFormatter()
-    {
-        BinaryFormatter formatter = new BinaryFormatter();
-        
-        return formatter;
-    }
+        public static BinaryFormatter GetBinaryFormatter()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
 
+            return formatter;
+        }
+
+    }
 }
