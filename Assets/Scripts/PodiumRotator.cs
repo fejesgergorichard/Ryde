74f25595589370990b87f9bb803a9750529899ad
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-09-15 00:10:40 +0200

    podium rotation

--- Assets/Scripts/PodiumRotator.cs
+++ Assets/Scripts/PodiumRotator.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PodiumRotator : MonoBehaviour
 {
     public float RotateSpeed = 10f;
-    private float startingPosition;
+    public int StepsToKillVelocity = 100;
+
+    private float touchPosLatest;
+    private float touchPosNew;
+
+    private Vector3 mousePosLatest;
+    private Vector3 mousePosNew;
+
+    private float velocity;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
 
     void Update()
     {
@@ -15,22 +30,59 @@ public class PodiumRotator : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    startingPosition = touch.position.x;
+                    touchPosLatest = touch.position.x;
                     break;
                 case TouchPhase.Moved:
-                    if (startingPosition > touch.position.x)
-                    {
-                        transform.Rotate(Vector3.down, -RotateSpeed * Time.deltaTime);
-                    }
-                    else if (startingPosition < touch.position.x)
-                    {
-                        transform.Rotate(Vector3.down, RotateSpeed * Time.deltaTime);
-                    }
+                    touchPosNew = touch.position.x;
+                    velocity = touchPosNew - touchPosLatest;
+                    touchPosLatest = touchPosNew;
                     break;
                 case TouchPhase.Ended:
+                    Task.Run(() => KillVelocity());
                     Debug.Log("Touch Phase Ended.");
                     break;
             }
+        }
+
+        // Mouse
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosLatest = Input.mousePosition;
+        }
+
+        else if (Input.GetMouseButton(0))
+        {
+            mousePosNew = Input.mousePosition;
+            velocity = (mousePosNew - mousePosLatest).x;
+            mousePosLatest = mousePosNew;
+        }
+
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Task.Run(() => KillVelocity());
+        }
+
+        if (velocity != 0)
+            transform.Rotate(Vector3.down, velocity * RotateSpeed * 0.001f);
+    }
+
+    private void KillVelocity()
+    {
+        var decreaseSpeed = velocity / StepsToKillVelocity;
+        var steps = 0;
+
+        while (velocity != 0)
+        {
+            if (steps == StepsToKillVelocity)
+            {
+                velocity = 0;
+                return;
+            }
+
+            velocity -= decreaseSpeed;
+            Thread.Sleep(20);
+            steps++;
         }
     }
 }
