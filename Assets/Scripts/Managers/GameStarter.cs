7003dc671bba79ebd35ead55208f493c66bb579a
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-09-14 23:38:26 +0200

    Initial MainMenu

--- Assets/Scripts/Managers/GameStarter.cs
+++ Assets/Scripts/Managers/GameStarter.cs
@@ -8,13 +8,17 @@ public class GameStarter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string targetScene = "Abstract1";
+        InitializeAppSettings();
 
+        string targetScene = "Abstract1";
         SceneManager.LoadScene(targetScene);
         SceneManager.LoadScene("Game", LoadSceneMode.Additive);
         GameManager.ActiveMap = targetScene;
         GameManager.Instance?.Restart();
+    }
 
+    private static void InitializeAppSettings()
+    {
         QualitySettings.vSyncCount = 0;
         QualitySettings.antiAliasing = 0;
 
