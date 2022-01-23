51ff107babab3980ba7009c6a8f5c3a9b0935f9b
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2022-01-23 21:10:48 +0100

    Editable tags for each breakable object

--- Assets/Scripts/ReplaceBodyOnCollision.cs
+++ Assets/Scripts/ReplaceBodyOnCollision.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ReplaceBodyOnCollision : MonoBehaviour
 {
     private bool _isBodyReplaced = false;
 
+    public List<string> TagsToBreakTo = new List<string>() { "Player" };
     public GameObject NormalBody;
     public GameObject ReplacementBody;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag == "Car" || collision.collider.tag == "Player")
+        if (TagsToBreakTo.Contains(collision.collider.tag))
         {
             if (!_isBodyReplaced)
             {
