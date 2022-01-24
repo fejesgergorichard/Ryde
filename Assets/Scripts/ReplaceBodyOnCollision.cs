85409fab822009b8a9bf9eedc305e33b1ec1d4a6
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2022-01-24 17:07:39 +0100

    Added breakable door to Abstract6

--- Assets/Scripts/ReplaceBodyOnCollision.cs
+++ Assets/Scripts/ReplaceBodyOnCollision.cs
@@ -8,15 +8,24 @@ public class ReplaceBodyOnCollision : MonoBehaviour
     public List<string> TagsToBreakTo = new List<string>() { "Player" };
     public GameObject NormalBody;
     public GameObject ReplacementBody;
+    public float VelocityConstraint = 0f;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (TagsToBreakTo.Contains(collision.collider.tag))
         {
-            if (!_isBodyReplaced)
+            Debug.Log(collision.relativeVelocity.magnitude);
+            if (collision.relativeVelocity.magnitude > VelocityConstraint)
             {
-                NormalBody.SetActive(false);
-                ReplacementBody.SetActive(true);
+                if (!_isBodyReplaced)
+                {
+                    NormalBody.SetActive(false);
+                    ReplacementBody.SetActive(true);
+                    if (gameObject.GetComponent<Tilter>())
+                    {
+                        gameObject.GetComponent<Tilter>().enabled = false;
+                    }
+                }
             }
         }
     }
